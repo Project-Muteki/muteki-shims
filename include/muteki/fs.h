0beeb5ecfa9343d5ad3aa92a6a562ed14866308d
dogtopus <dogtopus@users.noreply.github.com>
2021-10-26 07:29:05 -0300

    Full docs coverage on all APIs currently reversed.

--- include/muteki/fs.h
+++ include/muteki/fs.h
@@ -1,8 +1,19 @@
+/**
+ * @file fs.h
+ * @brief Filesystem access API.
+ */
+
 #ifndef __MUTEKI_FS_H__
 #define __MUTEKI_FS_H__
 
 #include <muteki/common.h>
 
+/**
+ * @brief Delete a file.
+ *
+ * @param pathname DOS 8.3 path to the file being removed.
+ * @return Whether or not the operation was successful.
+ */
 extern bool _aremove(const char *pathname);
 
 #endif // __MUTEKI_FS_H__
