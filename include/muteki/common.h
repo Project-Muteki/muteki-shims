6639aeb262dccd412296b36fcae288d2efcaa1cc
dogtopus <dogtopus@users.noreply.github.com>
2022-08-19 10:23:50 -0300

    common.h: Make UTF-16 definition more sound and add literal wrapper
    This would sacrifice a bit of compiler support but would be better on GCC-style compilers with different configurations.

--- include/muteki/common.h
+++ include/muteki/common.h
@@ -12,11 +12,27 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+// This only supports GCC and LLVM at the moment.
+#if defined(__CHAR16_TYPE__) && (__CHAR16_MAX__ < 0x10000)
+typedef __CHAR16_TYPE__ __BESTA_UTF_TYPE;
+#define __BESTA_UTF_LITERAL(x) (u ## x)
+#elif defined(__WCHAR_TYPE__) && (__WCHAR_MAX__ < 0x10000)
+typedef __WCHAR_TYPE__ __BESTA_UTF_TYPE;
+#define __BESTA_UTF_LITERAL(x) (L ## x)
+#else
+#error "Compiler does not support any 2 bytes wide string literal."
+#endif
+
 /**
- * @brief "Portable" UTF-16 type used by other headers.
+ * @brief "Portable" Besta UTF-16 type used by other headers.
  *
  * This is guaranteed to be 16-bit wide, unlike wchar_t (normally 32-bit wide) or char16_t (new C11 addition, can be >16-bit depending on the toolchain).
  */
-typedef uint16_t UTF16;
+#define UTF16 __BESTA_UTF_TYPE
+
+/**
+ * @brief Besta UTF-16 string literal.
+ */
+#define _BUL(x) __BESTA_UTF_LITERAL(x)
 
 #endif // __MUTEKI_COMMON_H__
