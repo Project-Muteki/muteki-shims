0836caf771f7e2fd54f15db7b581cb498b4d99e5
dogtopus <dogtopus@users.noreply.github.com>
2022-09-02 17:33:26 -0300

    errno.h: more docs

--- include/muteki/errno.h
+++ include/muteki/errno.h
@@ -14,20 +14,37 @@
 
 #include <muteki/common.h>
 
+/** Kernel error code type */
 typedef int32_t kerrno_t;
 
+/**
+ * @brief Flags for FormatMessage()
+ * @see https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-formatmessage
+ */
 enum message_flags_e {
+    /** Allocate buffer for the message. */
     FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x100,
+    /** Use system error message string table to generate the message. */
     FORMAT_MESSAGE_FROM_SYSTEM = 0x1000,
 };
 
+/**
+ * @brief Kernel errno namespace.
+ */
 enum errno_ns_flag_e {
+    /** User (errors set by OSSetLastError()). */
     ERRNO_NS_USER = 0x2,
+    /** Kernel. */
     ERRNO_NS_KERNEL = 0x4,
+    /** Executable loader. */
     ERRNO_NS_EXEC = 0x8,
+    /** Applet specific (errors set by _SetLastError()). */
     ERRNO_NS_APPLET_SPECIFIC = 0x2000,
 };
 
+/**
+ * @brief Error code for ::ERRNO_NS_EXEC.
+ */
 enum errno_exec_e {
     EXEC_UNSUPPORTED = 0x0001,
     EXEC_INVALID,
@@ -39,65 +56,126 @@ enum errno_exec_e {
     EXEC_UNKNOWN_FORMAT = 0x000a,
 };
 
+/**
+ * @brief Error code for ::ERRNO_NS_KERNEL.
+ */
 enum errno_kernel_e {
+    /** Peripheral communication failed. */
     PERIPHERAL_COMM_FAILED = 0x0003,
+    /** General hardware error. */
     GENERAL_HW_ERROR,
 
+    /** FTL: Data corruption detected. */
     FTL_DATA_CORRUPTED = 0x0010,
+    /** FTL: Data corruption detected and error correction attempt failed. */
     FTL_ECC_FAILED,
+    /** FTL: Data corruption detected and error correction attempt succeeded. */
     FTL_ECC_TRIGGERED = 0x0018,
+    /** FTL: Invalid Logical Block Address. */
     FTL_INVALID_LBA = 0x0021,
+    /** Medium is write protected. */
     MEDIUM_WP_ENABLED = 0x0027,
+    /** Medium changed */
     MEDIUM_CHANGED,
+    /** Medium is of an incompatible type. */
     MEDIUM_INCOMPATIBLE = 0x0030,
+    /** Medium I/O error. */
     MEDIUM_ERROR,
+    /** Medium is not loaded. */
     MEDIUM_UNLOADED = 0x003a,
 
+    /** Storage device not found. */
     SYS_STORAGE_DEVICE_NOT_FOUND = 0x0060,
+    /** Erase of storage device failed. */
     SYS_ERASE_FAILED,
+    /** Out of memory. */
     SYS_OUT_OF_MEMORY,
+    /** Low battery. */
     SYS_LOW_BATTERY,
+    /** Lock switch is on. System is locked. */
     SYS_LOCK_SWITCH_ENABLED,
 
+    /** Block device format prompt. */
     MKFS_PROMPT = 0x0102,
+    /** Block device needs formatting. */
     MKFS_UNSUPPORTED,
+    /** mkfs: No space left for device. */
     MKFS_NO_SPACE_LEFT,
+    /** mkfs: Mode error. */
     MKFS_MODE_ERROR,
+    /** mkfs: I/O error. */
     MKFS_IO_ERROR,
 
+    /** Invalid drive letter */
     FS_INVALID_DRIVE_LETTER = 0x0113,
 
+    /** Filename contains invalid characters. */
     FS_INVALID_FILENAME = 0x0140,
+    /** General filesystem I/O error. */
     FS_OPERATION_ERROR,
+    /** File or directory exists. */
     FS_ENTRY_EXISTS,
+    /** Too many files in this directory. */
     FS_DIR_FULL,
+    /** No such file or directory. */
     FS_NO_SUCH_ENTRY,
+    /** File/directory is not available. */
     FS_FILE_UNAVAILABLE,
+    /** Accessing file out of bound. */
     FS_FILE_OOB_ACCESS,
+    /** Conflicting file/directory attributes */
     FS_CONFLICTING_ATTR,
+    /** Too many open files. */
     FS_TOO_MANY_OPEN_FILES,
+    /** File/directory is locked for exclusive access. */
     FS_FILE_LOCKED,
+    /** Attribute error. */
     FS_FILE_ATTR_ERROR,
+    /** No space left for device. */
     FS_NO_SPACE_LEFT,
 
+    /** No such file or directory (alternative). */
     FS_NO_SUCH_ENTRY_ALT = 0x0154,
+    /** File is read-only. */
     FS_READ_ONLY_FILE = 0x0158,
+    /** Path too long. */
     FS_PATH_TOO_LONG = 0x0162,
+    /** Too many files in this directory (alternative). */
     FS_DIR_FULL_ALT1 = 0x0165,
+    /** Too many files in this directory (alternative). */
     FS_DIR_FULL_ALT2,
 
+    /** Database is corrupted. */
     DB_CORRUPTED = 0x0200,
+    /** Failed to open database. */
     DB_OPEN_FAILED,
+    /** Database index is full. Suggesting entry cleanup. */
     DB_INDEX_FULL_DELETE = 0x0203,
+    /** Database is full. */
     DB_FULL,
+    /** Database index is full. Suggesting sync with PC. */
     DB_INDEX_FULL_SYNC,
+    /** Database: too many open files. */
     DB_TOO_MANY_OPEN_FILES,
 };
 
+/**
+ * @brief Get the namespace portion of raw kernel errno value.
+ * @param kerrno Kernel errno value.
+ * @return The namespace.
+ * @see errno_ns_flag_e
+ */
 static inline unsigned short KERRNO_NS(kerrno_t kerrno) {
     return kerrno >> 16;
 }
 
+/**
+ * @brief Get the error code portion of raw kernel errno value.
+ * @param kerrno Kernel errno value.
+ * @return The error code.
+ * @see errno_exec_e
+ * @see errno_kernel_e
+ */
 static inline unsigned short KERRNO_ERR(kerrno_t kerrno) {
     return kerrno & 0xffff;
 }
@@ -134,9 +212,10 @@ extern kerrno_t OSGetLastError(void);
 /**
  * @brief Set errno.
  *
- * Unlike @p OSSetLastError, this also clears the kernel errno before calling @p OSSetLastError.
+ * Unlike OSSetLastError(), this also clears the kernel errno before calling OSSetLastError().
  *
- * If the errno namespace is unset, it will also automatically set @p ERRNO_NS_APPLET_SPECIFIC before passing it to @p OSSetLastError.
+ * If the errno namespace is unset, it will also automatically set ::ERRNO_NS_APPLET_SPECIFIC before passing it to
+ * OSSetLastError().
  *
  * Requires @p -lkrnllib when dynamically linking with the shims.
  *
@@ -147,7 +226,7 @@ extern void _SetLastError(kerrno_t err);
 /**
  * @brief Get errno.
  *
- * If kernel errno is set, the errno is returned with @p ERRNO_NS_KERNEL bit set. Otherwise it will return the result from @p OSSetLastError with @p ERRNO_NS_USER bit set.
+ * If kernel errno is set, the errno is returned with ::ERRNO_NS_KERNEL bit set. Otherwise it will return the result from OSSetLastError() with ::ERRNO_NS_USER bit set.
  *
  * Requires @p -lkrnllib when dynamically linking with the shims.
  *
