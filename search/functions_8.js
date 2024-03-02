e04fbf584035cfe6779b3a019d8d900fa5e4923d
dogtopus <dogtopus@users.noreply.github.com>
2024-03-02 04:11:18 +0000

    deploy: fb166b36ee892eb5fec017974bcc180f194ce18e

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['nand_5fget_5fparams_424',['nand_get_params',['../ftl_8h.html#a25716d0faad96ba43fee1c2c42390bb6',1,'ftl.h']]],
-  ['nand_5fread_5fpage_425',['nand_read_page',['../ftl_8h.html#a700fbb5979ba6e2fe260d324a34710a5',1,'ftl.h']]]
+  ['nand_5fget_5fparams_424',['nand_get_params',['../ftl_8h.html#afd4c3fb305847b79d0f97d61530ac579',1,'ftl.h']]],
+  ['nand_5fread_5fpage_425',['nand_read_page',['../ftl_8h.html#a7d59bb98342f5b462320cd05a2b51127',1,'ftl.h']]]
 ];
