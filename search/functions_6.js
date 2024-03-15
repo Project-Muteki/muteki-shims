be847ffeff8f1334f98bbecfca3e138c5dddf0ab
dogtopus <dogtopus@users.noreply.github.com>
2024-03-15 04:26:03 +0000

    deploy: dae2bc0ce855035cf073184844cefcef09a05113

--- search/functions_6.js
+++ search/functions_6.js
@@ -11,11 +11,12 @@ var searchData=
   ['getdrawarea_8',['GetDrawArea',['../canvas_8h.html#ae73f5af6932e361f5ecb50e9a6648fd9',1,'canvas.h']]],
   ['getevent_9',['GetEvent',['../event_8h.html#a9a41667de6a37b2a6b2c6fa1020197de',1,'event.h']]],
   ['getfontheight_10',['GetFontHeight',['../canvas_8h.html#a009e14c23044791c4250d8242ec811c4',1,'canvas.h']]],
-  ['getfreememory_11',['GetFreeMemory',['../memory_8h.html#a11e993980c4031eb09cafad85ddeb952',1,'memory.h']]],
-  ['getimagesize_12',['GetImageSize',['../canvas_8h.html#ababbf47f56c76a835a04e8709859bca0',1,'canvas.h']]],
-  ['getimagesizeext_13',['GetImageSizeExt',['../canvas_8h.html#a6657b1c8003221140be603fc3c696150',1,'canvas.h']]],
-  ['getmaxscrx_14',['GetMaxScrX',['../canvas_8h.html#a875cb08909fdf45af6fcb743e26e20b3',1,'canvas.h']]],
-  ['getmaxscry_15',['GetMaxScrY',['../canvas_8h.html#a2cc461b63c80d037a13699ad36de4c37',1,'canvas.h']]],
-  ['getsystime_16',['GetSysTime',['../datetime_8h.html#a32e84df2811a0547e643cd22097004f8',1,'datetime.h']]],
-  ['gettimer1inthandler_17',['GetTimer1IntHandler',['../system_8h.html#a77c18dab7a63ff188e452878d5907582',1,'system.h']]]
+  ['getfonttype_11',['GetFontType',['../canvas_8h.html#a7078127e59f0a75d66343edf2c7769fd',1,'canvas.h']]],
+  ['getfreememory_12',['GetFreeMemory',['../memory_8h.html#a11e993980c4031eb09cafad85ddeb952',1,'memory.h']]],
+  ['getimagesize_13',['GetImageSize',['../canvas_8h.html#ababbf47f56c76a835a04e8709859bca0',1,'canvas.h']]],
+  ['getimagesizeext_14',['GetImageSizeExt',['../canvas_8h.html#a6657b1c8003221140be603fc3c696150',1,'canvas.h']]],
+  ['getmaxscrx_15',['GetMaxScrX',['../canvas_8h.html#a875cb08909fdf45af6fcb743e26e20b3',1,'canvas.h']]],
+  ['getmaxscry_16',['GetMaxScrY',['../canvas_8h.html#a2cc461b63c80d037a13699ad36de4c37',1,'canvas.h']]],
+  ['getsystime_17',['GetSysTime',['../datetime_8h.html#a32e84df2811a0547e643cd22097004f8',1,'datetime.h']]],
+  ['gettimer1inthandler_18',['GetTimer1IntHandler',['../system_8h.html#a77c18dab7a63ff188e452878d5907582',1,'system.h']]]
 ];
