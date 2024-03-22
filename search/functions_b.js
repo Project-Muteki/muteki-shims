946833a0bf15c7abcafeae7ead4df4a60577d99f
dogtopus <dogtopus@users.noreply.github.com>
2024-03-22 02:49:00 +0000

    deploy: 887effdfafacd5528d5e828c0f08c238dc7d16c3

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['printf_0',['Printf',['../canvas_8h.html#a4bc499fc724de87725ee7dc60ed1d106',1,'canvas.h']]],
+  ['printf_0',['Printf',['../canvas_8h.html#a7365cbf5eae9bd5031842b29db13d370',1,'canvas.h']]],
   ['printfxy_1',['PrintfXY',['../canvas_8h.html#ae2bce93898870fbfd4541135b418c85c',1,'canvas.h']]]
 ];
