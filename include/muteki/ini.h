89fbbe3903c12e74805b1a637c2493de4c51fb3f
dogtopus <dogtopus@users.noreply.github.com>
2024-03-29 01:04:26 -0300

    ini: Document more Besta-specific behavior

--- include/muteki/ini.h
+++ include/muteki/ini.h
@@ -6,8 +6,11 @@
 /**
  * @file ini.h
  * @brief INI file operation API.
- * @todo This is mostly ripped from MSDN. Verification needed. Specifically some of the behaviors around `NULL`
- * handling.
+ * @details The functions in this API have the same names (other than the prefixed underscore) and function signatures
+ * as their
+ * [Win32 API counterpart](https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-getprivateprofileint)
+ * . However some behaviors in the original Win32 API were missing or altered, particularly on how these functions
+ * handle `NULL`s. Therefore one should not treat them in the same way as treating their Win32 API counterpart.
  */
 
 #ifndef __MUTEKI_INI_H__
@@ -25,7 +28,7 @@ extern "C" {
  * @param section The section where the key is located.
  * @param key The key.
  * @param default_value The default value to be returned when the key does not exist in the INI file.
- * @param path Path to INI file.
+ * @param path DOS 8.3 path to INI file.
  * @return The value that is associated with `section.key`, or `default` if the key does not exist.
  */
 extern unsigned int _GetPrivateProfileInt(const char *section, const char *key, int default_value, const char *path);
@@ -45,7 +48,7 @@ extern unsigned int _GetPrivateProfileInt(const char *section, const char *key, 
  * when set to `NULL`.
  * @param[out] out Pointer to the output buffer.
  * @param outsize Maximum size of the output buffer.
- * @param path Path to INI file.
+ * @param path DOS 8.3 path to INI file.
  * @return The length of the string copied to the `out` buffer.
  */
 extern unsigned int _GetPrivateProfileString(
@@ -58,12 +61,14 @@ extern unsigned int _GetPrivateProfileString(
 );
 
 /**
- * @brief Write a value to or remove a value from an INI file.
+ * @brief Write a value to an INI file.
+ * @details `NULL` in `section`, `key` or `value` will be interpreted as the string `"<NULL>"`. A `NULL`
+ * key but not a `NULL` section seems to also cause the function to stop using the specified section.
  * @x_syscall_num `0x10112`
  * @param section The section where the key is located.
- * @param key The key, or `NULL` to remove this section.
- * @param value The value, or `NULL` to remove this key.
- * @param path Path to INI file.
+ * @param key The key.
+ * @param value The value.
+ * @param path DOS 8.3 path to INI file.
  * @retval true @x_term ok
  * @retval false @x_term ng
  */
