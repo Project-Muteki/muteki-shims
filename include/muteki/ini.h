512de5ebd0a952637bddfd887cb8bad217a6c502
dogtopus <dogtopus@users.noreply.github.com>
2024-03-29 00:39:35 -0300

    ini: Warn user about potentially unchecked OOB in _GetPrivateProfileString

--- include/muteki/ini.h
+++ include/muteki/ini.h
@@ -33,12 +33,14 @@ extern unsigned int _GetPrivateProfileInt(const char *section, const char *key, 
 /**
  * @brief Read a value from an INI file.
  * @details For values, the function will copy at most `outsize - 1` bytes and the last byte in the `out` buffer will
- * always be set to <tt>'\0'</tt>. For listing names, the function will output at most `outsize - 2` bytes, with the
- * last 2 bytes always set to <tt>'\0'</tt> with the first one being a string terminator and the second one being a list
- * terminator.
+ * always be set to <tt>'\0'</tt>.
+ * @warning It is unsafe to use this function as not all systems check `outsize` and buffer overrun is not preventable
+ * on such systems.
+ * @note The section/key listing behavior as seen in the corresponding Win32 API call is unimplemented and setting
+ * `section` or `key` to `NULL` seems to only let the call fail unconditionally.
  * @x_syscall_num `0x10111`
- * @param section The section where the key is located, or `NULL` to list all available sections.
- * @param key The key, or `NULL` to list all available keys.
+ * @param section The section where the key is located.
+ * @param key The key.
  * @param default_value The default value to be copied to `out` when the key does not exist in the INI file, or `""`
  * when set to `NULL`.
  * @param[out] out Pointer to the output buffer.
