d4d797a6849ca6ea9c3e024d17b6f33382b3896c
dogtopus <dogtopus@users.noreply.github.com>
2024-03-28 22:53:04 -0300

    ini: Fix documentation
    Backticks paired with single quotes will be parsed as single quote pairs instead of inline code. Use <tt/> for this specific case instead.

--- include/muteki/ini.h
+++ include/muteki/ini.h
@@ -33,8 +33,9 @@ extern unsigned int _GetPrivateProfileInt(const char *section, const char *key, 
 /**
  * @brief Read a value from an INI file.
  * @details For values, the function will copy at most `outsize - 1` bytes and the last byte in the `out` buffer will
- * always be set to `'\0'`. For listing names, the function will output at most `outsize - 2` bytes, with the last 2
- * bytes always set to `'\0'` with the first one being a string terminator and the second one being a list terminator.
+ * always be set to <tt>'\0'</tt>. For listing names, the function will output at most `outsize - 2` bytes, with the
+ * last 2 bytes always set to <tt>'\0'<tt> with the first one being a string terminator and the second one being a list
+ * terminator.
  * @x_syscall_num `0x10111`
  * @param section The section where the key is located, or `NULL` to list all available sections.
  * @param key The key, or `NULL` to list all available keys.
