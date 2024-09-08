694901ba03ca7791c743d9cfe75652a9e8d9d5ae
dogtopus <dogtopus@users.noreply.github.com>
2024-09-08 16:43:44 +0000

    deploy: f64c13a4643d7a0a629d1e2a263791682786a23d

--- structui__event__base__s.js
+++ structui__event__base__s.js
@@ -1,11 +1,11 @@
 var structui__event__base__s =
 [
     [ "event_type", "structui__event__base__s.html#af4cabc2f81242d43a634af04ceb13829", null ],
-    [ "key_code0", "structui__event__base__s.html#a3357da20d62155baec1aee42499c1435", null ],
-    [ "key_code1", "structui__event__base__s.html#a6bd4e6152dcb317fbb873880cafa2dc1", null ],
+    [ "key_code0", "structui__event__base__s.html#aa6fc9869dd8c66ab75c6f21235fc1938", null ],
+    [ "key_code1", "structui__event__base__s.html#aa7777b9b61e5f5f69fbce55137979b0f", null ],
     [ "recipient", "structui__event__base__s.html#af59ae22188d83376bc07bb6f77202115", null ],
-    [ "touch_x", "structui__event__base__s.html#aa95dfa62b694e2cc4dc042140aa08e2d", null ],
-    [ "touch_y", "structui__event__base__s.html#abd12c563279b60dacee1f8f57ada33c3", null ],
+    [ "touch_x", "structui__event__base__s.html#a5b92e9448bf3c3b2c26672a0f5fd5693", null ],
+    [ "touch_y", "structui__event__base__s.html#a6792b48cfd00f0f9fb24683eec00c18e", null ],
     [ "unk16", "structui__event__base__s.html#af5f520b1307958f7d6f904787372049f", null ],
     [ "unk20", "structui__event__base__s.html#a6369ea883a5228a5b67c2ca75721d3bc", null ],
     [ "usb_data", "structui__event__base__s.html#af812bdcd50769ca5b7c09ec4e3ad58d8", null ]
