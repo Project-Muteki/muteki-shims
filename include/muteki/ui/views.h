26a294c5f784c68a89b39df2e431be8ba2256f5d
dogtopus <dogtopus@users.noreply.github.com>
2025-02-27 01:13:20 +0900

    ui/views: Update index

--- include/muteki/ui/views.h
+++ include/muteki/ui/views.h
@@ -6,6 +6,8 @@
 #ifndef __MUTEKI_UI_VIEWS_H__
 #define __MUTEKI_UI_VIEWS_H__
 
+#include <muteki/ui/views/deskbox.h>
+#include <muteki/ui/views/filepicker.h>
 #include <muteki/ui/views/messagebox.h>
 
 #endif // __MUTEKI_UI_VIEWS_H__
