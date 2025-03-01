5e87f45d3afa9f1576a82e96c3825c716de2f2c0
dogtopus <dogtopus@users.noreply.github.com>
2025-03-02 01:46:22 +0900

    fs: Add path helper syscalls

--- include/muteki/fs.h
+++ include/muteki/fs.h
@@ -22,7 +22,7 @@ extern "C" {
  * @details This includes the NUL terminator. Note that this can differ from libc definitions due to possible encoding
  * differences (e.g. newlib-muteki uses UTF-8 as the "ANSI" encoding by default).
  */
-#define SYS_PATH_MAX_CU 256
+#define SYS_PATH_MAX_CU 256u
 
 /**
  * @brief Structure for find context.
@@ -126,6 +126,28 @@ typedef struct {
  */
 #define FIND_TS_SECOND(ts) ((ts & 0xf) * 4)
 
+/** Maximum number of code units an LFN pathname can have including the trailing NUL. */
+#define FNSPLIT_LFN_PATHNAME_MAX 261u
+/** Maximum number of code units an LFN drive specifier can have including the trailing NUL. */
+#define FNSPLIT_LFN_DRIVE_MAX 4u
+/** Maximum number of code units an LFN directory name can have including the trailing NUL. */
+#define FNSPLIT_LFN_DIRNAME_MAX 257u
+/** Maximum number of code units an LFN base name can have including the trailing NUL. */
+#define FNSPLIT_LFN_BASENAME_MAX 257u
+/** Maximum number of code units an LFN file suffix can have including the trailing NUL. */
+#define FNSPLIT_LFN_SUFFIX_MAX 259u
+
+/** Maximum number of code units an DOS 8.3 pathname can have including the trailing NUL. */
+#define FNSPLIT_DOS_PATHNAME_MAX 81u
+/** Maximum number of code units a DOS 8.3 drive specifier can have including the trailing NUL. */
+#define FNSPLIT_DOS_DRIVE_MAX 4u
+/** Maximum number of code units a DOS 8.3 directory name can have including the trailing NUL. */
+#define FNSPLIT_DOS_DIRNAME_MAX 67u
+/** Maximum number of code units a DOS 8.3 base name can have including the trailing NUL. */
+#define FNSPLIT_DOS_BASENAME_MAX 10u
+/** Maximum number of code units a DOS 8.3 file suffix can have including the trailing NUL. */
+#define FNSPLIT_DOS_SUFFIX_MAX 9u
+
 /**
  * @brief File/directory attributes.
  * @details Just the attributes in FAT32.
@@ -149,6 +171,7 @@ enum fs_attribute_e {
 
 /**
  * @brief Delete a file.
+ * @x_syscall_num `0x100dd`
  * @param pathname DOS 8.3 path to the file being removed.
  * @retval true @x_term ok
  * @retval false @x_term ng
@@ -157,6 +180,7 @@ extern bool _aremove(const char *pathname);
 
 /**
  * @brief Delete a file.
+ * @x_syscall_num `0x10274`
  * @param pathname UTF-16 LFN path to the file being removed.
  * @retval true @x_term ok
  * @retval false @x_term ng
@@ -167,6 +191,8 @@ extern bool __wremove(const UTF16 *pathname);
  * @brief Begin a file/directory search.
  * @details Matches the first file/directory based on fnmatch pattern `fnmatch` and attribute mask `attrib_mask`. Saves
  * context at `ctx`, which can be used later with _afindnext() and _findclose().
+ * 
+ * @x_syscall_num `0x100d8`
  * @param fnmatch The pattern.
  * @param ctx The context object.
  * @param attrib_mask Attribute mask (use 0 to match everything regardless of attributes).
@@ -178,6 +204,7 @@ extern short _afindfirst(const char *fnmatch, find_context_t *ctx, int attrib_ma
 /**
  * @brief Continue a previously started file/directory search.
  * @details Matches the next file/directory.
+ * @x_syscall_num `0x100d9`
  * @param ctx The context object.
  * @retval 0 There is a match.
  * @retval -1 No more match was found.
@@ -188,6 +215,8 @@ extern short _afindnext(find_context_t *ctx);
  * @brief Begin a file/directory search (LFN support).
  * @details Matches the first file/directory based on fnmatch pattern `fnmatch` and attribute mask `attrib_mask`. Saves
  * context at `ctx`, which can be used later with _afindnext() and _findclose().
+ * 
+ * @x_syscall_num `0x10270`
  * @param fnmatch The pattern (encoded in UTF-16).
  * @param ctx The context object.
  * @param attrib_mask Attribute mask (use 0 to match everything regardless of attributes).
@@ -204,6 +233,7 @@ extern short _wfindfirst(const UTF16 *fnmatch, find_context_t *ctx, int attrib_m
  * Not sure if interchangeable with _afindnext() since it should have no dependency on different find context. Needs to
  * be verified later.
  *
+ * @x_syscall_num `0x10271`
  * @param ctx The context object.
  * @retval 0 There is a match.
  * @retval -1 No more match was found.
@@ -212,6 +242,7 @@ extern short _wfindnext(find_context_t *ctx);
 
 /**
  * @brief Terminate a file/directory search.
+ * @x_syscall_num `0x100da`
  * @param ctx The context object.
  * @retval 0 @x_term ok
  */
@@ -220,6 +251,7 @@ extern int _findclose(find_context_t *ctx);
 /**
  * @brief Get attributes of specific path.
  * @details Similar to GetFileAttributeW() in Windows.
+ * @x_syscall_num `0x10272`
  * @param path UTF-16 LFN path.
  * @return The attribute, or -1 on error.
  * @see fs_attribute_e
@@ -229,6 +261,7 @@ extern short _wfgetattr(UTF16 *path);
 /**
  * @brief Set attributes of specific path.
  * @details Similar to SetFileAttributeW() in Windows.
+ * @x_syscall_num `0x10273`
  * @param path UTF-16 LFN path.
  * @param attrs The new attribute.
  * @return The new attribute, or -1 on error.
@@ -239,6 +272,7 @@ extern short _wfsetattr(UTF16 *path, short attrs);
 /**
  * @brief Get attributes of specific path.
  * @details Similar to GetFileAttributeA() in Windows.
+ * @x_syscall_num `0x100db`
  * @param path DOS 8.3 path.
  * @return The attribute, or -1 on error.
  * @see fs_attribute_e
@@ -248,6 +282,7 @@ extern short _afgetattr(char *path);
 /**
  * @brief Set attributes of specific path.
  * @details Similar to SetFileAttributeA() in Windows.
+ * @x_syscall_num `0x100dc`
  * @param path DOS 8.3 path.
  * @param attrs The new attribute.
  * @return The new attribute, or -1 on error.
@@ -257,6 +292,7 @@ extern short _afsetattr(char *path, short attrs);
 
 /**
  * @brief Create a new directory.
+ * @x_syscall_num `0x10277`
  * @param path UTF-16 LFN path to the new directory.
  * @retval 0 @x_term ok
  * @retval -1 @x_term ng
@@ -265,6 +301,7 @@ extern int _wmkdir(UTF16 *path);
 
 /**
  * @brief Create a new directory.
+ * @x_syscall_num `0x100e0`
  * @param path DOS 8.3 path to the new directory.
  * @retval 0 @x_term ok
  * @retval -1 @x_term ng
@@ -273,6 +310,7 @@ extern int _amkdir(char *path);
 
 /**
  * @brief Remove a directory.
+ * @x_syscall_num `0x10278`
  * @details Sets kerrno namespace to ::ERRNO_NS_KERNEL and error code to `0xffff` when directory is not empty.
  * @param path UTF-16 LFN path to the directory being removed.
  * @retval 0 @x_term ok
@@ -282,6 +320,7 @@ extern int _wrmdir(UTF16 *path);
 
 /**
  * @brief Remove a directory.
+ * @x_syscall_num `0x100e1`
  * @details Sets kerrno namespace to ::ERRNO_NS_KERNEL and error code to `0xffff` when directory is not empty.
  * @param path DOS 8.3 path to the directory being removed.
  * @retval 0 @x_term ok
@@ -291,6 +330,7 @@ extern int _armdir(char *path);
 
 /**
  * @brief Get system CWD value.
+ * @x_syscall_num `0x100e3`
  * @param unk Some context object, format unknown. Use NULL to get the CWD of current applet.
  * @param buf Buffer large enough to contain a DOS8.3 path. Must be at least ::SYS_PATH_MAX_CU bytes long.
  * @retval 0 @x_term ok
@@ -300,6 +340,7 @@ extern short _agetcurdir(void *unk, char *buf);
 
 /**
  * @brief Get system CWD value.
+ * @x_syscall_num `0x1027a`
  * @param unk Some context object, format unknown. Use NULL to get the CWD of current applet.
  * @param buf Buffer large enough to contain a UTF-16 LFN path. Must be at least ::SYS_PATH_MAX_CU units long.
  * @retval 0 @x_term ok
@@ -309,6 +350,7 @@ extern short _wgetcurdir(void *unk, UTF16 *buf);
 
 /**
  * @brief Change system working directory to a new path.
+ * @x_syscall_num `0x100e2`
  * @param path DOS 8.3 path.
  * @retval 0 @x_term ok
  * @retval -1 @x_term ng
@@ -317,6 +359,7 @@ extern short _achdir(const char *path);
 
 /**
  * @brief Change system working directory to a new path.
+ * @x_syscall_num `0x10279`
  * @param path UTF-16 LFN path.
  * @retval 0 @x_term ok
  * @retval -1 @x_term ng
@@ -325,6 +368,7 @@ extern short _wchdir(const UTF16 *path);
 
 /**
  * @brief Rename/move a file.
+ * @x_syscall_num `0x100de`
  * @param old_path Old DOS 8.3 path.
  * @param new_path New DOS 8.3 path.
  * @retval 0 @x_term ok
@@ -334,6 +378,7 @@ extern short _arename(const char *old_path, const char *new_path);
 
 /**
  * @brief Rename/move a file.
+ * @x_syscall_num `0x10275`
  * @param old_path Old UTF-16 LFN path.
  * @param new_path New UTF-16 LFN path.
  * @retval 0 @x_term ok
@@ -343,11 +388,95 @@ extern short _wrename(const UTF16 *old_path, const UTF16 *new_path);
 
 /**
  * @brief Get filesystem usage stats.
+ * @x_syscall_num `0x100eb`
  * @param fsid Filesystem ID.
  * @param fs_stat Target to store filesystem usage stats.
  * @retval 0 @x_term ok
  */
 extern int FSGetDiskRoomState(int fsid, fs_stat_t *fs_stat);
+
+/**
+ * @brief Split a DOS 8.3 pathname into parts.
+ * @details DOS 8.3 counterpart of _wfnsplit().
+ * @param[in] pathname Pathname to be split.
+ * @param[out] drive Drive name, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_DOS_DRIVE_MAX.
+ * @param[out] dirname Directory, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_DOS_DIRNAME_MAX.
+ * @param[out] basename Basename without suffix, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_DOS_BASENAME_MAX.
+ * @param[out] suffix Suffix, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_DOS_SUFFIX_MAX.
+ * @return Bitfield that indicates which parts were being extracted successfully.
+ * @see _wfnsplit Its LFN counterpart.
+ */
+extern int _afnsplit(const char *pathname, char *drive, char *dirname, char *basename, char *suffix);
+
+/**
+ * @brief Build an DOS 8.3 pathname from parts.
+ * @details DOS 8.3 counterpart of _wfnmerge().
+ * @param[out] pathname Pointer to a buffer where the constructed pathname will be stored.
+ * This buffer must be at least as long as FNSPLIT_DOS_PATHNAME_MAX.
+ * @param[in] drive Drive specifier (e.g., `C` or `C:`). If provided, it must be a valid drive name.
+ * @param[in] dirname Directory path (e.g., `\path\to\a\`). If provided, it must end with a backslash.
+ * @param[in] basename Base name without a suffix (e.g., `file`).
+ * @param[in] suffix File suffix (e.g., `.txt`).
+ * @return Bitfield that indicates which parts were added into the resulting pathname.
+ * @see _wfnmerge Its LFN counterpart.
+ */
+extern int _afnmerge(char *pathname, const char *drive, const char *dirname, const char *basename, const char *suffix);
+
+/**
+ * @brief Split an LFN pathname into parts.
+ * @details
+ * Borland libc style path parsing helper. It takes a pathname in the format of `C:\path\to\a\file.txt` and parse
+ * it into 4 parts:
+ * - drive: The drive name consists of the drive letter and a single colon (`C:`)
+ * - dirname: The directory name with the root backslash and training backslash (`\path\to\a\`)
+ * - basename: Basename without the suffix (`file`)
+ * - suffix: Suffix with the leading dot (`.txt`)
+ * 
+ * Borland C++ has a documentation regarding to their implementation:
+ * https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Fnsplit,_wfnsplit.
+ *
+ * @x_syscall_num `0x1026b`
+ * @param[in] pathname Pathname to be split.
+ * @param[out] drive Drive name, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_LFN_DRIVE_MAX.
+ * @param[out] dirname Directory, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_LFN_DIRNAME_MAX.
+ * @param[out] basename Basename without suffix, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_LFN_BASENAME_MAX.
+ * @param[out] suffix Suffix, or NULL to omit this part.
+ * Must be at least as long as FNSPLIT_LFN_SUFFIX_MAX.
+ * @return Bitfield that indicates which parts were being extracted successfully.
+ */
+extern int _wfnsplit(const UTF16 *pathname, UTF16 *drive, UTF16 *dirname, UTF16 *basename, UTF16 *suffix);
+
+/**
+ * @brief Build an LFN pathname from parts.
+ * @details
+ * Borland libc style path parsing helper. It constructs a pathname by combining the components: drive, directory,
+ * basename, and suffix. It is designed to be the inverse of the _wfnsplit() syscall, allowing users to reconstruct a
+ * full pathname from its individual parts.
+ *
+ * Each component is optional and can be provided as `NULL` if it is not needed in the final pathname. The syscall
+ * will concatenate the non-NULL components in the order of drive, directory, basename, and suffix, ensuring proper
+ * formatting such as adding backslashes where necessary.
+ *
+ * Borland C++ has a documentation regarding to their implementation:
+ * https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Fnmerge,_wfnmerge.
+ *
+ * @x_syscall_num `0x1026c`
+ * @param[out] pathname Pointer to a buffer where the constructed pathname will be stored.
+ * This buffer must be at least as long as FNSPLIT_LFN_PATHNAME_MAX.
+ * @param[in] drive Drive specifier (e.g., `C` or `C:`). If provided, it must be a valid drive name.
+ * @param[in] dirname Directory path (e.g., `\path\to\a\`). If provided, it must end with a backslash.
+ * @param[in] basename Base name without a suffix (e.g., `file`).
+ * @param[in] suffix File suffix (e.g., `.txt`).
+ * @return Bitfield that indicates which parts were added into the resulting pathname.
+ */
+extern int _wfnmerge(UTF16 *pathname, const UTF16 *drive, const UTF16 *dirname, const UTF16 *basename, const UTF16 *suffix);
 
 #ifdef __cplusplus
 } // extern "C"
