dc7279aa2ba53774c5c5802d5f34275d9281aae6
dogtopus <dogtopus@users.noreply.github.com>
2025-06-30 01:43:49 +0900

    syscall: Fix minor errors.
    The function has now been verified on emulator.

--- src/syscall.s
+++ src/syscall.s
@@ -12,7 +12,7 @@ syscall:
     mov r2, r3
     @ If passing 3+ arguments
     tst r0, #0x80000000
-    bne _syscall_insert_lr
+    beq _syscall_insert_lr
     @ x a1 a2 x | sysno a0 a3 ...
     push {r0}
     @ a0 a1 a2 x | sysno x a3 ...
@@ -22,17 +22,19 @@ syscall:
     @ x a1 a2 a3 | sysno x a0 ...
     str r0, [sp, #8]
     @ sysno a1 a2 a3 | x x a0 ...
-    ldr r0, [sp, #0]
+    ldr r0, [sp]
     @ sysno a1 a2 a3 | a0 ...
-    sub sp, #8
+    add sp, sp, #8
     @ Fall through
+
 _syscall_insert_lr:
     @ sysno a1 a2 a3? | lr a0 ...
     push {lr}
 
     bic r0, r0, #0xff000000
     orr r0, r0, #0xef000000
-    @ TODO build the SVC instruction
+
+    @ sysno a1 a2 a3? | (svc sysno) lr a0 ...
     str r0, [sp, #-4]
     sub r0, sp, #4
     bx r0
